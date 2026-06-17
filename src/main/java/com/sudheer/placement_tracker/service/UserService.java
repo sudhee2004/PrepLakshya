6641b45f2f6cfeccae5aca9aefdd5aeb76e16d12
sudhee2004 <sudheersudheer16704@gmail.com>
2026-06-17 11:52:19 +0530

    Remove application.properties from git tracking

--- src/main/java/com/sudheer/placement_tracker/service/UserService.java
+++ src/main/java/com/sudheer/placement_tracker/service/UserService.java
@@ -3,6 +3,7 @@ package com.sudheer.placement_tracker.service;
 import com.sudheer.placement_tracker.model.User;
 import com.sudheer.placement_tracker.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,17 +12,22 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
     public User registerUser(User user) {
         if (userRepository.findByEmail(user.getEmail()).isPresent()) {
             throw new RuntimeException("Email already exists!");
         }
+        // Encrypt password before saving
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
     public User loginUser(String email, String password) {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new RuntimeException("User not found!"));
-        if (!user.getPassword().equals(password)) {
+        // Compare entered password with encrypted password
+        if (!passwordEncoder.matches(password, user.getPassword())) {
             throw new RuntimeException("Invalid password!");
         }
         return user;
